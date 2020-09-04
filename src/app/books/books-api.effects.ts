12cac1c2083c3b46a8856fb8ac4e4cefeeb42818
Jack Parra Murillo <jack.parra@sinco.com.co>
2020-09-04 06:35:48 -0500

    add crud effects

--- src/app/books/books-api.effects.ts
+++ src/app/books/books-api.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType } from "@ngrx/effects";
-import { mergeMap, map } from "rxjs/operators";
+import { mergeMap, map, exhaustMap, concatMap } from "rxjs/operators";
 import { BooksService } from "../shared/services";
 import { BooksPageActions, BooksApiActions } from "./actions";
 
@@ -8,14 +8,47 @@ import { BooksPageActions, BooksApiActions } from "./actions";
 export class BookApiEffects {
     constructor(private bookService: BooksService, private actions$: Actions){}
 
-        loadBooks$ = createEffect(() => 
-            this.actions$.pipe(
-                ofType(BooksPageActions.enter),
-                mergeMap(() => 
-                    this.bookService
-                        .all()
-                        .pipe(map(books => BooksApiActions.booksLoaded({ books })))
-                )
-            )   
-        );    
+    loadBooks$ = createEffect(() => 
+        this.actions$.pipe(
+            ofType(BooksPageActions.enter),
+            exhaustMap(() => 
+                this.bookService
+                    .all()
+                    .pipe(map(books => BooksApiActions.booksLoaded({ books })))
+            )
+        )   
+    );    
+
+    createBook$ = createEffect(() => 
+        this.actions$.pipe(
+            ofType(BooksPageActions.createBook),
+            concatMap(action => 
+                this.bookService
+                .create(action.book)
+                .pipe(map(book => BooksApiActions.bookCreated({ book })))
+            )
+        )
+    );
+
+    updateBook$ = createEffect(() => 
+        this.actions$.pipe(
+            ofType(BooksPageActions.updateBook),
+            concatMap(action =>
+                this.bookService
+                .update(action.bookId, action.changes)
+                .pipe(map(book => BooksApiActions.bookUpdated({ book })))
+            )
+        )
+    );
+
+    deleteBook$ = createEffect(() => 
+        this.actions$.pipe(
+            ofType(BooksPageActions.deleteBook),
+            mergeMap(action =>
+                this.bookService
+                .delete(action.bookId)
+                .pipe(map(() => BooksApiActions.bookDeleted({ bookId: action.bookId })))
+            )
+        )
+    );
 }
